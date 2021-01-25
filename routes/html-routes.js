d18112ff46ee8ff988abebeedf464501f84efb62
Sally Perez <seattlesally1@gmail.com>
2021-01-25 14:56:10 -0800

    updated html routes, will need to update once move to public and handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,4 +8,25 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 // =============================================================
 module.exports = function(app){
 
+    // index route
+    app.get("/login", function(req, res) {
+        // res.sendFile(path.join(__dirname, "../public/login.html"));
+        res.sendFile(path.join(__dirname, "../login.html"));
+    });
+
+    app.get("/signup", function(req, res) {
+        // res.sendFile(path.join(__dirname, "../public/signup.html"));
+        res.sendFile(path.join(__dirname, "../signup.html"));
+    });
+
+    app.get("/profile", function(req, res) {
+        // res.sendFile(path.join(__dirname, "../public/profile.html"));
+        res.sendFile(path.join(__dirname, "../profile.html"));
+    });
+
+    app.get("*", function(req, res) {
+        // res.sendFile(path.join(__dirname, "../public/login.html"));
+        res.sendFile(path.join(__dirname, "../login.html"));
+    });
+
 };
