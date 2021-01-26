656abbf942b3da5375d33a83d41f3271032e43a1
Lauren Plenger <laurenb08@gmail.com>
2021-01-25 20:58:55 -0800

    converting HTML to hanlebars

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
 
-db.sequelize.sync({ force: true }).then(function() {
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    })
-  }).catch((err) => { throw err });
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  })
+}).catch((err) => { throw err });
