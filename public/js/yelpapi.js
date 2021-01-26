c280bda94bc72b7eb7f3b473dfffc2000b02a050
elliefh <elliefu18@gmail.com>
2021-01-25 20:48:10 -0800

    switched axios to ajax

--- public/js/yelpapi.js
+++ public/js/yelpapi.js
@@ -1,31 +1,12 @@
-
-
-let API_KEY = "QpL1_euY18mZRFtPkAvPGAV-qzoLh7iZN7zRUo6gjv-JSCrVL69NghanELXxl84TuWj_pNQisuQcp1_zG73BCFSuW7LZHPMJwPvRmYOvWQRma_YiRBCtp_-_4Y8MYHYx"
-
-const userSearch = ""
-// REST
-let yelpREST = axios.create({
-  baseURL: "https://api.yelp.com/v3/",
-  headers: {
-    Authorization: `Bearer ${API_KEY}`,
-    "Content-type": "application/json",
+var settings = {
+  “url”: “https://api.yelp.com/v3/businesses/search?term=delis&categories=vegetarian&location=Seattle&limit=10”,
+  “method”: “GET”,
+  “timeout”: 0,
+  “headers”: {
+    “Authorization”: “Bearer QpL1_euY18mZRFtPkAvPGAV-qzoLh7iZN7zRUo6gjv-JSCrVL69NghanELXxl84TuWj_pNQisuQcp1_zG73BCFSuW7LZHPMJwPvRmYOvWQRma_YiRBCtp_-_4Y8MYHYx”
   },
-})
+};
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 
-yelpREST(ENDPOINT, { params: { key: value } }).then(({ data }) => {
-  // Do something with the data
-})
-
-// Using the yelpREST helper we defined earlier
-yelpREST("/businesses/search/seattle", {
-    params: {
-      location: "seattle",
-      term: 
-    },
-  }).then(({ data }) => {
-    let { businesses } = data
-    businesses.forEach((b) => {
-      console.log("Name: ", b.name)
-    })
-  })
-  
