487c5c9b2f7e72a8e10c484ef6f9d0e5ed4f62a9
elliefh <elliefu18@gmail.com>
2021-01-28 21:48:43 -0800

    added margin space for see suggestions button and setting up handlebars using yelp api response data

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -7,7 +7,7 @@ const API_KEY = process.env.DB_APIKEY
 
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-const { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } = require("constants");
+// const { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } = require("constants");
 
 // Routes
 // =============================================================
@@ -33,10 +33,16 @@ const { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } = require("constants");
                 Authorization: `Bearer ${API_KEY}`,
             }
         }).then((results) => {
+            let restaurants = JSON.stringify(results.data.business);
 
             console.log(results.data.businesses);
+            console.log(results.data.businesses[0].name);
+            console.log(results.data.businesses[0].rating);
+            console.log(results.data.businesses[0].image_url);
+            console.log(results.data.businesses[0].display_phone);
+            console.log(results.data.businesses[0].url);
             res.render("profile", {user: req.user});
-
+            res.render("profile", restaurants);
         });
 
         // res.sendFile(path.join(__dirname, "../public/profile.html"));
